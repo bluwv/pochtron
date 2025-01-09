18f210df7ea778e2d9401f0803a7315245b361e0
bluwv <adrienpierre@aol.fr>
2025-01-09 20:22:44 +0100

    first commit

---
+++ httpdocs/admin/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pochtron</title>
+	<link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+<body class="admin login">
+
+	<div id="app">
+		<main class="site-main">
+			<section class="login-box">
+				<h1>Welcome Back</h1>
+				<p>Don’t have an account yet? <a href="#">Sign up</a></p>
+
+				<form method="POST" action="../app/login.php" novalidate>
+					<label for="username">email address</label>
+					<input id="username" type="email" name="username" placeholder="name@gmail.com" required>
+
+					<label for="password">password</label>
+					<input  id="password" type="password" name="password" placeholder="******" required>
+
+					<input type="submit" value="Login">
+					<button type="submit">Login</button>
+				</form>
+
+				<a href="#">I lost password</a>
+			</section>
+		</main>
+	</div>
+
+</body>
+</html>
---
+++ httpdocs/index.php
@@ -0,0 +1,24 @@
+<?php
+header('Location : catalogue.php');
+die();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php include_once 'views/layouts/header.php'; ?>
+
+	<main class="site-main">
+		index
+	</main>
+
+	<?php include_once 'views/layouts/footer.php'; ?>
+
+</body>
+</html>
